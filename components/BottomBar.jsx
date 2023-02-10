15c58971aaa9af5252d608f5c391f795a59b5962
mayur-chace <mayur.kamthe@lockrfinance.com>
2023-02-10 10:47:13 +0530

    Ui changes updated

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -6,21 +6,23 @@ import music from '../images/musical.png'
 import news from '../images/news.png'
 
 function BottomBar() {
+    const barBg = 'flex border w-screen h-[70px] fixed bottom-0 z-1 bg-indigo-500'
+    const barStyle = 'flex flex-col w-[25%] h-full justify-center items-center cursor-pointer bg-indigo-500 hover:bg-indigo-400';
   return (
-    <div className='flex border w-screen h-[70px] fixed bottom-0 z-1 bg-indigo-500'>
-        <div className='flex flex-col w-[25%] h-full justify-center items-center cursor-pointer bg-indigo-500 hover:bg-indigo-400'>
+    <div className={barBg}>
+        <div className={barStyle}>
             <div><Image src={heart} width={20} height={20}/></div>
             <div>Favorites</div>
         </div>
-        <div className='flex flex-col w-[25%] h-full justify-center items-center bg-indigo-500 cursor-pointer hover:bg-indigo-400'>
+        <div className={barStyle}>
         <div><Image src={music} width={20} height={20}/></div>
             <div>Music</div>
         </div>
-        <div className='flex flex-col w-[25%] h-full justify-center items-center bg-indigo-500 cursor-pointer hover:bg-indigo-400'>
+        <div className={barStyle}>
         <div><Image src={location} width={20} height={20}/></div>
             <div>Places</div>
         </div>
-        <div className='flex flex-col w-[25%] h-full justify-center items-center bg-indigo-500 cursor-pointer hover:bg-indigo-400'>
+        <div className={barStyle}>
         <div><Image src={news} width={20} height={20}/></div>
             <div>News</div>
         </div>
